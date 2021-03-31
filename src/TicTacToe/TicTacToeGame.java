03f376388953071e5f87273eaa50450dba46d26a
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-31 11:16:50 +0530

    ADDED: Created Tic Tac Toe Game board

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -1,7 +1,22 @@
 package TicTacToe;
 
 public class TicTacToeGame {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
+
+		char[][] gameBoard = { { ' ', '|', ' ', '|', ' ' }, { '-', '+', '-', '+', '-' }, { ' ', '|', ' ', '|', ' ' },
+				{ '-', '+', '-', '+', '-' }, { ' ', '|', ' ', '|', ' ' } };
+
+		printGameBoard(gameBoard);
+
+	}
+	//created a tic tac toe game board
+	public static void printGameBoard(char[][] gameBoard) {
+		for (char[] row : gameBoard) {
+			for (char c : row) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
 	}
 }
